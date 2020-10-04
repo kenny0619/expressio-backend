01bea818999bfb040e175f64e8b8f23d63982ca6
kenny0619 <kehindeibukun0619@gmail.com>
2020-10-04 14:44:12 +0100

    setup passport and jwt middleware

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+require("./db/mongoose");
+require("./models/User");
+require("./config/passport");
+
+// const indexRouter = require("./routes/index");
+// const usersRouter = require("./routes/users");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(require("./routes"));
+
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
 
 module.exports = app;
---
+++ config/passport.js
@@ -0,0 +1,27 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const { model } = require("mongoose");
+const User = model("User");
+
+//Configure Passport using the passport-local strategy
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "user[email]",
+      passwordField: "user[password]",
+    },
+    (email, password, done) => {
+      User.findOne({ email: email })
+        .then((user) => {
+          if (!user || !user.validPassword(password)) {
+            return done(null, false, {
+              errors: { "email or password": "is invalid" },
+            });
+          }
+
+          return done(null, user);
+        })
+        .catch(done);
+    }
+  )
+);
