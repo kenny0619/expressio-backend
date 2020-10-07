42640c488101c227cca89c68ed15c82998ae27c0
kenny0619 <kehindeibukun0619@gmail.com>
2020-10-07 15:16:19 +0100

    handled session storage

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-require("./db/mongoose");
-require("./models/User");
-require("./config/passport");
+const session = require("express-session");
 
 const app = express();
 
@@ -39,6 +36,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret: "expressio",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+require("./db/mongoose");
+require("./models/User");
+require("./config/passport");
 
 app.use(require("./routes"));
 
